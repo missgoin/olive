9b2153ebaee36abb1b29af4bae49eae679e077a5
Nathan Chancellor <nathan@kernel.org>
2022-10-13 11:12:49 -0700

    ANDROID: kbuild: Wrap zero trivial-auto-var-init enablement flag with cc-option
    Starting with LLVM 16, this flag is deprecated, which means its presence
    breaks the build:
    
      Cannot use CONFIG_CC_STACKPROTECTOR_STRONG: -fstack-protector-strong not supported by compiler
      make[2]: *** [../Makefile:1289: prepare-compiler-check] Error 1
      clang-16: warning: the flag '-enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang' has been deprecated and will be ignored [-Wunused-command-line-argument]
      clang-16: warning: the flag '-enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang' has been deprecated and will be ignored [-Wunused-command-line-argument]
    
    Wrap this option in cc-option so that it only gets added to the list of
    flags when it is supported. This is resolved properly upstream already
    with commit 607e57c6c62c ("hardening: Remove Clang's enable flag for
    -ftrivial-auto-var-init=zero") but that cannot be easily backported due
    to lack of cc-option support in Kconfig, hence this downstream only
    change.
    
    Change-Id: Ie1f7bcdec23f01bf19c68b4dae460923aaa150c9
    Fixes: a7eda6eddca5 ("BACKPORT: security: allow using Clang's zero initialization for stack variables")
    Link: https://github.com/llvm/llvm-project/commit/aef03c9b3bed5cef5a1940774b80128aefcb4095
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,91 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Cherry Kernel V2.7.5 - by @AkiraNoSushi & @Flopster101
+do.devicecheck=1
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=pine
+device.name2=olive
+device.name3=olivelite
+device.name4=olivewood
+device.name5=mi439
+device.name6=olives
+device.name7=Mi439
+supported.versions=11,12,13
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+setprop audio.volume 84
+setprop audio.play_wav_file "$(pwd)/easter_egg.wav"
+
+# R/W dynamic partitions fix
+if [ -d "/dev/block/mapper" ]; then
+    blockdev --setrw /dev/block/mapper/system
+    blockdev --setrw /dev/block/mapper/vendor
+fi
+
+## AnyKernel install
+split_boot;
+
+ui_print "Mounting /vendor..."
+mount -o rw,remount /vendor
+
+vndk_version=$(file_getprop /vendor/build.prop ro.vendor.build.version.sdk)
+
+if [ $vndk_version -lt 30 ]; then
+    abort "Unsupported VNDK version. Aborting..."
+fi
+
+ui_print "Detecting WiFi HAL..."
+
+wifi_hal=$(find /vendor/lib64 /vendor/lib -name "libwifi-hal.so" | head -n 1)
+if grep -q "pronto_wlan.ko" $wifi_hal; then
+    abort "Unsupported WiFi HAL. Aborting..."
+fi
+
+ui_print "Detecting camera HAL..."
+
+if [[ ! -z "$(find /vendor/lib64 /vendor/lib -name '*lib2d*')" ]]; then
+    patch_cmdline "prebuilt_camera_hal" "prebuilt_camera_hal"
+else
+    patch_cmdline "prebuilt_camera_hal" ""
+fi
+
+flash_boot;
+flash_dtbo;
+
+ui_print " "
+
+ui_print "Mounting /system..."
+mount -o rw,remount /system
+
+## Patches
+# Prevent init from overriding kernel tweaks.
+ui_print "Patching system's init..."
+# IMO this is kinda destructive but works
+find /system/etc/init/ -type f | while read file; do 
+sed -Ei 's;[^#](write /proc/sys/(kernel|vm)/(sched|dirty|perf_cpu|page-cluster|stat|swappiness|vfs));#\1;g' $file
+done
+# Prevent init from overriding ZRAM algorithm
+ui_print "Patching vendor's init..."
+remove_line "/vendor/etc/init/hw/init.qcom.rc" "comp_algorithm" "global"
+# Replace post_boot with ours.
+ui_print "Pushing init.qcom.post_boot.sh..."
+replace_file "/vendor/bin/init.qcom.post_boot.sh" "0755" "init.qcom.post_boot.sh"
+## end install
