0ea7d29e0670d8ef4970ded18aaaf884eedddb12
missgoin <mister.goindo@gmail.com>
2023-06-11 20:04:15 +0700

    add local

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,8 +4,8 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Cherry Kernel V2.7.5 - by @AkiraNoSushi & @Flopster101
-do.devicecheck=1
+kernel.string=SUPER.KERNEL
+do.devicecheck=0
 do.modules=0
 do.systemless=0
 do.cleanup=1
